ce86e4da3f8fb20bf474329d536210e0405f0d26
Denis Dreano <denis.dreano@protonmail.ch>
2023-12-22 18:24:02 +0100

    Send multiple lines to R via file (tmux paste not working nice with R console)

--- ftplugin/quarto.lua
+++ ftplugin/quarto.lua
@@ -9,3 +9,12 @@ vim.wo.showbreak = "|"
 
 -- don't run vim ftplugin on top
 vim.api.nvim_buf_set_var(0, "did_ftplugin", true)
+
+vim.cmd(
+  [[
+  function! _EscapeText_quarto(text)
+    let out = _EscapeText_r(a:text)
+    return out
+  endfunction
+  ]]
+)
--- ftplugin/r.lua
+++ ftplugin/r.lua
@@ -1,1 +1,15 @@
 vim.b.slime_cell_delimiter = "#\\s\\=%%"
+
+vim.cmd(
+  [[
+  function! _EscapeText_r(text)
+    if len(split(a:text, "\n")) > 1
+      let cwd = getcwd()
+      call system("cat > " . cwd . "/.slime_r", a:text)
+      return ["\rsource('" . cwd . "/.slime_r', echo = TRUE, max.deparse.length = 4095, spaced = FALSE)\r\r"]
+    else
+      return [a:text]
+    endif
+  endfunction
+  ]]
+)
